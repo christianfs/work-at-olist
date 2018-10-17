de1d39aff26b22b858269c76e38bd7817c35f5da
Christian Segovia <christianfs@gmail.com>
2018-10-17 17:20:20 -0300

    Create base app
    Create a simple server.

---
+++ call_records/api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import Blueprint
+from flask_restful import Api
+from flask_script import Manager
+
+
+def create_app():
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+def register_blueprints(app):
+    blueprint = Blueprint('api', __name__)
+    api = Api(blueprint)
+
+
+if __name__ == "__main__":
+    app = create_app()
+    manager = Manager(app)
+    manager.run()
+    app.run()
